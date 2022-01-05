f9093dc9d643bb3ed1fd4469601dd853900f84d5
ljnsn <lbj@goja.me>
2022-01-05 22:33:55 +0100

    separate db url and path

--- fastr/config.py
+++ fastr/config.py
@@ -8,9 +8,18 @@ class Settings(BaseSettings):
     Override example:
     `export FASTR_SECRET_KEY=secret`
     """
-    database_path: str = "./fastr.sqlite"
+
+    database_url: str = "sqlite+aiosqlite:///"
+    database_path: str = "fastr.sqlite"
     secret_key: str = "<override_in_production>"
 
     class Config:
         env_prefix = "fastr_"
         env_file = ".env"
+
+    @property
+    def connectstring(self):
+        return self.database_url + self.database_path
+
+
+settings = Settings()
